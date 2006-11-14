fbfdf5519d18d6dae3dfcf72c9e0974140764a56
mattias <mattias.bogeblad@knowit.se>
2006-11-14 20:59:40 +0000

    Worked more on event versions

--- docs/upgrade1.3_1.4.sql
+++ docs/upgrade1.3_1.4.sql
@@ -5,24 +5,23 @@ CREATE TABLE language (
   PRIMARY KEY  (id)
 );
 
-CREATE TABLE event_version (
-  id BIGINT unsigned NOT NULL auto_increment,
-  name varchar(255) default NULL,
-  description varchar(255) default NULL,
-  lecturer varchar(1024) default NULL,
-  longDescription text NULL,
-  contactEmail varchar(255) default NULL,
-  shortDescription text NULL,
-  organizerName varchar(255) default NULL,
-  contactPhone varchar(255) default NULL,
-  price float default NULL,
-  customLocation varchar(255) default NULL,
-  eventUrl varchar(255) default NULL,
-  contactName varchar(255) default NULL,
-  event_id int default NULL,
-  attributes text NULL,
-  PRIMARY KEY  (id)
-);
+DROP TABLE IF EXISTS `calendar`.`event_version`;
+CREATE TABLE  `calendar`.`event_version` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `description` varchar(255) default NULL,
+  `lecturer` varchar(1024) default NULL,
+  `longDescription` text,
+  `shortDescription` text,
+  `contactPhone` varchar(255) default NULL,
+  `customLocation` varchar(255) default NULL,
+  `eventUrl` varchar(255) default NULL,
+  `event_id` int(11) default NULL,
+  `attributes` text,
+  `alternativeLocation` varchar(255) default NULL,
+  `language_id` bigint(20) unsigned default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS Calendar_Language;
 CREATE TABLE  Calendar_Language (
